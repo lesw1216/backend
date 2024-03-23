2d4816796cfe685e3592b453652b2f8e9dc1bdef
sangwoo <lesw1216@gmail.com>
2024-03-23 19:37:22 +0900

    setting: 프로젝트 초기화

---
+++ src/test/java/com/swoo/fitlog/FitLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swoo.fitlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
