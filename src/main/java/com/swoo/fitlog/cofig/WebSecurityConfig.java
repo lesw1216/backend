da0921e446ddcdd0887236750e24f11946793746
sangwoo <lesw1216@gmail.com>
2024-03-26 16:23:28 +0900

    setting: security csrf 설정, url 권한 설정
    /api/v1/auth/locals/email 권한 모두 접근으로 변경

---
+++ src/main/java/com/swoo/fitlog/cofig/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.swoo.fitlog.cofig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                authorize -> authorize
+                        .requestMatchers("/api/v1/auth/locals/email").permitAll()
+                        .anyRequest().authenticated()
+        );
+
+        /*
+        * CSRF disable
+        * */
+        http.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
+
+        return http.build();
+    }
+}
