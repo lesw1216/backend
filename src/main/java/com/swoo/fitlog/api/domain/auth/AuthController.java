50b60c4c49968fc15ad7ebdc71e08abcbd36cfc7
sangwoo <lesw1216@gmail.com>
2024-03-27 11:29:44 +0900

    feat: 인증 번호 유효 시간 조회 API 구현

--- src/main/java/com/swoo/fitlog/api/domain/auth/AuthController.java
+++ src/main/java/com/swoo/fitlog/api/domain/auth/AuthController.java
@@ -2,13 +2,16 @@ package com.swoo.fitlog.api.domain.auth;
 
 import com.swoo.fitlog.api.domain.auth.dto.EmailDto;
 import com.swoo.fitlog.api.domain.auth.service.MailSendService;
+import com.swoo.fitlog.api.domain.auth.service.OtpService;
+import com.swoo.fitlog.api.http.RestApiResponse;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -16,18 +19,38 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final MailSendService mailSendService;
+    private final OtpService otpService;
 
     /*
         이메일 수신 후 인증 번호 발송
      */
     @PostMapping("/api/v1/auth/locals/email")
-
-    public String receiveEmail(@Valid @RequestBody EmailDto emailDto) throws MessagingException {
+    public ResponseEntity<Object> receiveEmail(@Valid @RequestBody EmailDto emailDto) throws MessagingException {
         String email = emailDto.getEmail();
         log.debug("[E-mail]=[{}]", email);
 
         mailSendService.send(email);
 
-        return "success";
+        RestApiResponse<Object> restApiResponse = RestApiResponse.builder()
+                .code(200)
+                .httpStatus(HttpStatus.OK)
+                .message("이메일 인증 번호 발송")
+                .build();
+
+        return new ResponseEntity<>(restApiResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/v1/auth/locals/email/expiration")
+    public ResponseEntity<Object> sendExpirationTime(@RequestParam("email") String email) {
+        Long expiration = otpService.getExpiration(email);
+
+        RestApiResponse<Object> restApiResponse = RestApiResponse.builder()
+                .code(200)
+                .httpStatus(HttpStatus.OK)
+                .message("유효 시간 획득 성공")
+                .data(expiration)
+                .build();
+
+        return new ResponseEntity<>(restApiResponse, HttpStatus.OK);
     }
 }
